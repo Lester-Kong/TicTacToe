7acf18a8278960daff26c8ff5aa9ea771f33341f
Lester-Kong <lkong001121@gmail.com>
2024-09-28 13:42:33 -0400

    added testing for win conditions

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,22 +4,37 @@ def print_board(board):
         print(" | ".join(row))
     print("-" * 10)
 
+def check_winner(board):
+    for row in board:       #Check each row for win condition
+        if(all(x == row[0] and x != " " for x in row)):
+            return True
+        
+    col_list = []           #Check each column for win condition
+    for col in range(len(board[0])): 
+        for row in board:
+            col_list.append(row[col])
+        if(all(x == col_list[0] and x != " " for x in col_list)):
+            return True
+        else:
+            col_list.clear()
+
+    if(board[0][0] == board[1][1] == board[2][2] != " "):   #Check both diagonals for win condition
+        return True
+    elif(board[0][2] == board[1][1] == board[2][0] != " "):
+        return True
+        
+    return False
+
 def tic_tac_toe():
     row_coord_dict = {
-        'T' : 0,    #Top
-        't' : 0,
-        'M' : 1,    #Middle
-        'm' : 1,
-        'B' : 2,    #Bottom
-        'b' : 2}
+        't' : 0,    #Top
+        'm' : 1,    #Middle
+        'b' : 2}    #Bottom
     
     col_coord_dict = {
-        'L' : 0,    #Left
-        'l' : 0,
-        'M' : 1,    #Middle
-        'm' : 1,
-        'R' : 2,    #Right
-        'r' : 2}
+        'l' : 0,    #Left
+        'm' : 1,    #Middle
+        'r' : 2}    #Right
     board = [[" " for i in range(3)] for j in range(3)]
     current_player = "X"
 
@@ -27,18 +42,27 @@ def tic_tac_toe():
         print_board(board)
 
         print(f"Player {current_player}, enter your desired row. Use T/t for top, M/m for middle, B/b for bottom")
-        row = input()
-        while(row not in ['T', 't', 'M', 'm', 'B', 'b']):
+        row = input().casefold()
+        while(row not in ['t', 'm', 'b']):
             row = input("That selection was invalid, please pick another one.")
         row = row_coord_dict[row]
         
         print(f"Player {current_player}, enter your desired column. Use L/l for left, M/m for middle, R/r for right.")
-        col = input()
-        while(col not in ['L', 'l', 'M', 'm', 'R', 'r']):
+        col = input().casefold()
+        while(col not in ['l', 'm', 'r']):
             col = input("That selection was invalid, please pick another one.")
         col = col_coord_dict[col]
 
-        print(row, col)
+        if(board[row][col] == " "):
+            board[row][col] = current_player
+        else:
+            print("That spot is already taken, please choose another one.")
+            continue
+
+        if(check_winner(board)):
+            print("Winner")
+        else:
+            print("Not yet")
 
 if __name__ == "__main__":
     tic_tac_toe()
