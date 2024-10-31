f26ebaa1fe0235230b2eb5decd1c22c6c033a9c0
Lester Kong <lkong001121@gmail.com>
2024-10-31 17:07:37 -0400

    Added ending messages

--- TicTacToe.py
+++ TicTacToe.py
@@ -42,13 +42,13 @@ def tic_tac_toe():
         print_board(board)
 
         print(f"Player {current_player}, enter your desired row. Use T/t for top, M/m for middle, B/b for bottom")
-        row = input().casefold()
+        row = input().casefold()            #Convert input to lowercase
         while(row not in ['t', 'm', 'b']):
             row = input("That selection was invalid, please pick another one.")
         row = row_coord_dict[row]
         
         print(f"Player {current_player}, enter your desired column. Use L/l for left, M/m for middle, R/r for right.")
-        col = input().casefold()
+        col = input().casefold()            #Convert input to lowercase
         while(col not in ['l', 'm', 'r']):
             col = input("That selection was invalid, please pick another one.")
         col = col_coord_dict[col]
@@ -60,9 +60,22 @@ def tic_tac_toe():
             continue
 
         if(check_winner(board)):
-            print("Winner")
-        else:
-            print("Not yet")
+            print_board(board)
+            print(f"Player {current_player} is the winner!")
+            print("Press enter to close the program.")
+            input()
+            break
+        if all(board[row][col] != " " for row in range(3) for col in range (3)):
+            print_board(board)
+            print("The game ends in a tie!")
+            print("Press enter to close the program.")
+            input()
+            break
+
+        if(current_player == "X"):
+            current_player = "O"
+        elif(current_player == "O"):
+            current_player = "X"
 
 if __name__ == "__main__":
     tic_tac_toe()
